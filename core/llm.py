a876b6d72c1f5bd9b1d3e3af02cf9b33d826922b
abhinav-kurup <abhinavkurup2002@gmail.com>
2026-05-25 07:06:28 +0530

    added GROQ provider

--- core/llm.py
+++ core/llm.py
@@ -1,4 +1,5 @@
 from langchain_community.chat_models import ChatOllama
+from langchain_groq import ChatGroq
 import os
 
 
@@ -12,6 +13,7 @@ def get_llm(model_identifier: str, temperature: float = 0.2, timeout: int = 30):
       - "ollama/llama3"
     If no prefix is provided, defaults to ollama.
     """
+    print(model_identifier)
     if model_identifier.startswith("gemini/"):
         from langchain_google_genai import ChatGoogleGenerativeAI
         model_name = model_identifier.split("gemini/")[1]
@@ -28,6 +30,13 @@ def get_llm(model_identifier: str, temperature: float = 0.2, timeout: int = 30):
             base_url=base_url,
             temperature=temperature,
             timeout=timeout
+        )
+    elif model_identifier.startswith("groq/"):
+        model_name = model_identifier.split("groq/")[1]
+        base_url = os.getenv("OLLAMA_BASE_URL", "http://localhost:11434")
+        return ChatGroq(
+            model="llama-3.3-70b-versatile",  # or "llama-3.1-8b-instant"
+            temperature=0
         )
     else:
         base_url = os.getenv("OLLAMA_BASE_URL", "http://localhost:11434")
